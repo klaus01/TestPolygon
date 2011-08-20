e227dccbcb54992e00dff48e320f075812475881
kelei <kelei@keleimatoMac-Pro.local>
2011-08-20 15:15:38 +0800

    Initial Commit

---
+++ TestPolygon/TestPolygonAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TestPolygonAppDelegate.h
+//  TestPolygon
+//
+//  Created by kelei on 11-8-20.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestPolygonAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
