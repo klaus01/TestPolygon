1b3dc4922d429653f277fe10e952ed82414b5aef
kelei <kelei@keleimatoMac-Pro.local>
2011-08-23 21:28:49 +0800

    优化加载配置时机

--- TestPolygon/MainViewController.m
+++ TestPolygon/MainViewController.m
@@ -14,21 +14,6 @@
 
 static NSString *UD_NumberOfSides = @"NumberOfSides";
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    //从数据文件中加载边数配置
-    polygonView.NumberOfSides = [[NSUserDefaults standardUserDefaults] integerForKey:UD_NumberOfSides];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
@@ -38,11 +23,18 @@ static NSString *UD_NumberOfSides = @"NumberOfSides";
             || interfaceOrientation == UIInterfaceOrientationLandscapeRight);
 }
 
-//自定义方法
+//视图即将可见时调用
+- (void)viewWillAppear:(BOOL)animated
+{
+    //从数据文件中加载边数配置
+    polygonView.NumberOfSides = [[NSUserDefaults standardUserDefaults] integerForKey:UD_NumberOfSides];
+}
+
+//将边数保存到配置文件中
 - (void)saveUserDataNumberOfSides:(int)_numberOfSides
 {
-    //将边数保存到配置文件中
-    //注意：设置后不会立即保存到文件中，当应用退出时才保存。如果应用异常结束，那么数据将不会被保存。但可以手动调用[[NSUserDefaults standardUserDefaults] synchronize]来保存
+    //注意：设置后不会立即保存到文件中，当应用退出时才保存。如果应用异常结束，那么数据将不会被保存。
+    //     但可以手动调用[[NSUserDefaults standardUserDefaults] synchronize]来保存
     [[NSUserDefaults standardUserDefaults] setInteger:_numberOfSides forKey:UD_NumberOfSides];
 }
 
