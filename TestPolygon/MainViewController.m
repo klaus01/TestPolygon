86df841cb493447d8a00c26f2bb9e849beb310b1
klaus01 <kelei0017@gmail.com>
2011-08-23 18:01:38 +0800

    +保存配置

--- TestPolygon/MainViewController.m
+++ TestPolygon/MainViewController.m
@@ -12,9 +12,13 @@
 
 @implementation MainViewController
 
+static NSString *UD_NumberOfSides = @"NumberOfSides";
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    //从数据文件中加载边数配置
+    polygonView.NumberOfSides = [[NSUserDefaults standardUserDefaults] integerForKey:UD_NumberOfSides];
     // Do any additional setup after loading the view from its nib.
 }
 
@@ -35,12 +39,21 @@
 }
 
 //自定义方法
+- (void)saveUserDataNumberOfSides:(int)_numberOfSides
+{
+    //将边数保存到配置文件中
+    //注意：设置后不会立即保存到文件中，当应用退出时才保存。如果应用异常结束，那么数据将不会被保存。但可以手动调用[[NSUserDefaults standardUserDefaults] synchronize]来保存
+    [[NSUserDefaults standardUserDefaults] setInteger:_numberOfSides forKey:UD_NumberOfSides];
+}
+
 - (void)btnIncClick:(id)sender
 {
-    polygonView.NumberOfSides += 1;
+    polygonView.NumberOfSides++;
+    [self saveUserDataNumberOfSides:polygonView.NumberOfSides];
 }
 - (void)btnDecClick:(id)sender
 {
-    polygonView.NumberOfSides -= 1;
+    polygonView.NumberOfSides--;
+    [self saveUserDataNumberOfSides:polygonView.NumberOfSides];
 }
 @end
